b08fca5f48389f5e42a4096e049af5b88788d112
Doritos0 <meyesblue@gmail.com>
2024-10-25 04:00:06 -0300

    uwu #2 //FALTA NORMALIZAR INPUTS

--- core/views.py
+++ core/views.py
@@ -10,10 +10,11 @@ lineal = load('./modelos/linear_model.pkl')
 
 
 
+
 def home(request):
 
-    
-    prediction = None
+    regre = None
+    clasi = None
 
     if request.method == 'POST':
         if 'regresion' in request.POST:
@@ -32,16 +33,36 @@ def home(request):
 
                 print("ESTA ES LA PRED QUE LLEGA ", pred)
                 prediction = pred[0]
-                request.session['prediction'] = prediction  # Guardamos la predicción para mostrarla en el contexto
+                request.session['prediction'] = prediction 
 
-                prediction = request.session.get('prediction', None)
+                regre = request.session.get('prediction', None)
 
             except Exception as e:
                 print("Error en la predicción: ", e)
                 prediction = "Error en la entrada de datos."
 
+        elif 'clasificacion' in request.POST:
+            try:
+                data = {
+                    'Edad': [float(request.POST['edad'])],
+                    'Internauta': [float(request.POST['internauta'])],
+                    'Ctacte': [float(request.POST['cuenta_cte'])],
+                    'Debito': [float(request.POST['debito'])],
+                    'CambioPin': [float(request.POST['cambio_pin'])],
+                    'Cuentas': [float(request.POST['cuentas'])],
+                    'TC': [float(request.POST['tc'])]
+                }
+                input_df = pd.DataFrame(data)
+                pred = random.predict(input_df)
+                clasi = pred[0]
 
-    return render(request, 'index.html', { 'pred' : prediction})
+            except Exception as e:
+                print("Error en la predicción de clasificación: ", e)
+                clasi = "Error en la entrada de datos."
+
+
+
+    return render(request, 'index.html', { 'regre' : regre, 'clasi' : clasi })
 
 
 '''
@@ -52,4 +73,16 @@ TC: -0.7399128501103254 (float64)
 CUPO_MX: 0.9020878457232103
 
 L1 REAL: 1.4446278462946316
+'''
+
+'''
+Index(['Edad', 'Internauta', 'Ctacte', 'Debito', 'CambioPin', 'Cuentas', 'TC'
+
+Edad: 0.323
+Internauta: 1
+Cta Cte: 1
+Débito: 1
+Cambio PIN: 0.0
+Cuentas: -0.74
+TC: 1.445
 '''
